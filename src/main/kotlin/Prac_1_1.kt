8309256443f48a3ab6ca382b450591d7e2b4ca49
Pulkid <pulkidmewada21@gnu.ac.in>
2023-07-25 11:42:08 +0530

    1.1

--- src/main/kotlin/Prac_1_1.kt
+++ src/main/kotlin/Prac_1_1.kt
@@ -1,6 +1,22 @@
 fun main(){
-    println("Hello world")
+    val a:Int=2;
+    val b:Float=6f;
+    val c:Char='P';
+    val h:String= "Pulkid";
+    val d:Double=23.65;
+    val e:Long=846562158;
+    val f:Short=-9;
+    val g: Byte=104;
+    val i:Boolean=true;
 
-    println("Hello world")
-    println("Hello world")
+    println("Integer Value:"+a)
+    println("Float Value:"+b)
+    println("Char Value:"+c)
+    println("String Value:"+h)
+    println("Double Value:"+d)
+    println("Long Value:"+e)
+    println("Short Value:"+f)
+    println("Byte Value:"+g)
+    println("Boolean Value:"+i)
+
 }
