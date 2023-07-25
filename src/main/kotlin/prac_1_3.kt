00e663e0a693c6cec3ebc0b7dbd0f90650643273
Pulkid <pulkidmewada21@gnu.ac.in>
2023-07-25 12:31:46 +0530

    1.3

---
+++ src/main/kotlin/prac_1_3.kt
@@ -0,0 +1,53 @@
+fun main(){
+    var a="Student Enrollment:"
+    print("$a")
+    var b=readLine()
+
+    var c="Student Name:"
+    print("$c")
+    var d=readLine()
+
+    var e="Student Age:"
+    print("$e")
+    var f=readLine()
+
+    var g="Student Branch:"
+    print("$g")
+    var h=readLine()
+
+    var i="Student Class:"
+    print("$i")
+    var j=readLine()
+
+    var k="Student Batch:"
+    print("$k")
+    var l=readLine()
+
+    var m="Student College Name:"
+    print("$m")
+    var n=readLine()
+
+    var o="Student University Name:"
+    print("$o")
+    var p=readLine()
+
+
+    println("\n*************************\n")
+
+    print("Enrollment: ")
+    println("$b")
+    print("Name: ")
+    println("$d")
+    print("Age: ")
+    println("$f")
+    print("Branch: ")
+    println("$h")
+    print("Class: ")
+    println("$j")
+    print("Batch: ")
+    println("$l")
+    print("College Name: ")
+    println("$n")
+    print("University Name: ")
+    println("$p")
+}
