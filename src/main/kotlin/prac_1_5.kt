f0b9886d671720a63e2e510ead8c02353d3870c6
Pulkid <pulkidmewada21@gnu.ac.in>
2023-08-01 10:32:50 +0530

    1.5

---
+++ src/main/kotlin/prac_1_5.kt
@@ -0,0 +1,19 @@
+fun main(){
+    println("Enter month in number: ");
+    var a= readLine()!!.toInt();
+    when(a){
+        1-> println("January");
+        2-> println("Febuary");
+        3-> println("March");
+        4-> println("April");
+        5-> println("May");
+        6-> println("June");
+        7-> println("July");
+        8-> println("August");
+        9-> println("September");
+        10-> println("October");
+        11-> println("November");
+        12-> println("December");
+
+    }
+}
